def837f5892aba8309088a2c30945a0e2390ee7a
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-10-14 17:46:20 -0400

    fix

--- main.ts
+++ main.ts
@@ -2,7 +2,11 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program lights up a single led
 */
 
-basic.showString('Hello, World!')
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+basic.showIcon(IconNames.Happy)
